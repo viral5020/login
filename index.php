35be75444ae431bf9ae1ed0d8c6935b5b418bbe1
viral5020 <66018491+viral5020@users.noreply.github.com>
2020-09-02 17:50:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Form site</title>
+<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+<form method="post" action="connect.php">
+Username : <input type="text" name="username"><br><br>
+Password : <input type="password" name="password"><br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
